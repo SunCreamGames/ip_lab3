2fcce66bf44953cfce4c2e9146449c2201a91525
Ann <anna.kurennaya38@gmail.com>
2020-03-17 14:11:24 +0200

    reading from cmd

---
+++ lab3/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+//C:\Users\Ann\labs_IP\lab3\lab3
+//"C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\Roslyn\csc.exe"
+
+namespace lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(args[0] + args[2]);
+        }
+    }
+}
